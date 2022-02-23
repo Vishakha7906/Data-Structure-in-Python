a19140b6088463c632a02b21239cbce78d56c1cd
VISHAKHA SAINI <vishakhasaini7906@gmail.com>
2022-02-23 17:13:05 +0530

    Add files via upload

---
+++ Largest_number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def largestNumber(self, num):
+             def compare(n1, n2):
+            if n1+n2 > n2+n1:
+                return 1
+            elif n1+n2 < n2+n1:
+                return -1
+            else:
+                return 0
+
+        num_str = [str(n) for n in num]
+        res = ""
+
+        # Sorting according to customized function
+        for n in reversed( sorted(num_str,cmp=compare) ):
+            res += n
+
+        # Remove unnecessary zeros in head
+        res_list = list(res)
+        i = 0 
+        while res_list[i] == '0' and i != len(res)-1:
+            i += 1
+        res_list = res_list[i:]
+
+        return ''.join( res_list)
