ddd8f7ab97b44811d24aee7d2bac1bb0f017787e
VISHAKHA SAINI <vishakhasaini7906@gmail.com>
2022-04-15 10:55:28 +0530

    Add files via upload

---
+++ contains duplicate.py
@@ -0,0 +1,7 @@
+def containsDuplicate(self, nums: List[int]) -> bool:
+        s=set()
+        for ele in nums:
+            if ele in s:
+                return True
+            else:
+                s.add(ele)
